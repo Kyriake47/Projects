6c084dc56c13b4844d04b1b8d7dab6cd842921d3
Dominika <129898211+Kyriake47@users.noreply.github.com>
2023-04-10 00:18:24 +0200

    Text file

--- plik tekstowy/Plik tekstowy/Source.cpp
+++ plik tekstowy/Plik tekstowy/Source.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include <iomanip>
+
+// Program przepisuje wartoœci z pliku txt do tablicy, grupuj¹c je, wykrywaj¹c brakuj¹ce wartoœci
 
 using namespace std;
-int i = 1;//numer wiersza
+int i =1;//numer wiersza
 void Read(string table[][3]);
-int AddToTable(string table[][3], string a, string b, int i, int j, int k);
+int AddToTable(string table[][3], string a, string b, int *i, int j, int k);
 
 int main() {
     string table[10][3];
-	//string** wsk = &table[0];
+	
 	table[0][0] = "Miasto";
 	table[0][1] = "Imie";
 	table[0][2] = "Nazwisko";
@@ -29,77 +32,86 @@ int main() {
 	cout << table[2][1] << endl;
 	cout << table[0][2] << endl;
 	cout << table[2][2] << endl;
+
+	cout << table[0][2] << endl;
+	cout << table[3][2] << endl;
+
+
+
 	return 0;
 }
 
-int AddToTable(string table[][3], string a, string b, int i, int j, int k) {
+int AddToTable(string table[][3], string a, string b, int &i, int j, int k) {
 	if (a == "Miasto") {
 		j = 1;
-		if (k <= j) {		
-			table[i][0] = b;
-			cout << "i=" << i << endl;
-			//cout << table[*i][0] << endl;
+		if (k < j) {		
+			table[i][0] = b;		
 		}
 		else {
 			i++;
-			table[i][0] = b;
-			cout << table[i][0] << endl;
-			//cout << "i=" << *i << endl;
+			table[i][0] = b;		
 		}
 	}
 	if (a == "Imie") {
 		j = 2;
-		if (k <= j) {
-			table[i][1] = b;
-			cout << "i=" << i << endl;
-			//cout << table[*i][0] << endl;
+		if (k < j) {
+			table[i][1] = b;			
 		}
 		else {
 			i++;
-			table[i][1] = b;
-			cout << table[i][0] << endl;
-			//cout << "i=" << *i << endl;
+			table[i][1] = b;			
 		}
 	}
 	if (a == "Nazwisko") {
 		j = 3;
-		if (k <= j) {
-			table[i][2] = b;
-			cout << "i=" << i << endl;
-			//cout << table[*i][0] << endl;
+		if (k < j) {
+			table[i][2] = b;			
 		}
 		else {
 			i++;
-			table[i][2] = b;
-			cout << table[i][0] << endl;
-			//cout << "i=" << *i << endl;
+			table[i][2] = b;			
 		}
 	}
-	//funkcja int wiec przypisanie wartosci wskaznika, i zapisanie go pod k, j jest wskaznikiem wiec zmienia siê ca³y czas
+	
 	return j;
 
 }
 void Read(string table[][3]) {
 
 	string wyraz1;
-	string wyraz2;
+	 string wyraz2;
 	int j = 0;//numer kolumny
 	
 	int k = 0;//przepisanie j do porównania
 	fstream plik;
-	plik.open("tabela.txt", ios::in);
+	plik.open("tabela.txt", ios::in|ios::out);
 	cout << "Plik otwiera sie" << endl;
+
+//string linia_tekstu;
+//int dlugosc;
+//	while (!plik.eof()) {
+		
+//		getline(plik, linia_tekstu);
+		//dlugosc = linia_tekstu.length();
+		//linia_tekstu += " ";
+//		std::cout <<":" << linia_tekstu << "\n";
+		//linia_tekstu.insert(dlugosc, " ");
+//		plik << linia_tekstu;
+//	}
+	
 
 	while (!plik.eof()) {
 
 		getline(plik, wyraz1, '=');
-		//cout << wyraz1 << endl;
-								// ' ' lub '\n'
-			getline(plik, wyraz2, ' ');
-			//if(wyraz2.include('n'))
-			wyraz2=wyraz2.replace(wyraz2.size(), "\n", "");
 		
-		//cout << wyraz2;
+			getline(plik, wyraz2, ' ');
+
+			while (wyraz1.find_first_of('\n') != -1) {
+				wyraz1.replace(wyraz1.find_first_of('\n'), 1, "");
+			}
+
+
+			
 		k=AddToTable(table, wyraz1, wyraz2, i, j, k);
 		j = k;
 	}
