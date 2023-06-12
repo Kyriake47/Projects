33a50f697ceb4ef8dc84e11c87715b67994c2fa0
Dominika <129898211+Kyriake47@users.noreply.github.com>
2023-06-12 23:51:18 +0200

    Update AGD

--- AGD/logowanie.php
+++ AGD/logowanie.php
@@ -8,71 +8,102 @@ include("polaczenie.php");
 	$login = mysqli_real_escape_string($conn,$_POST["login"]);
   $haslo = mysqli_real_escape_string($conn,$_POST["password"]);
  
-    
-    $sql = "SELECT * FROM Users
-    WHERE Login=? AND Password=?";
-    $stmt=mysqli_prepare($conn,$sql);
-    mysqli_stmt_bind_param($stmt,'ss',$login,$haslo);
+  try{
+$sql="SELECT Password FROM users WHERE Login='$login'";
+$res = $conn -> query($sql);
+$row=mysqli_fetch_array($res);
+}
+catch(Exception $e){
+  echo 'zla nazwa użytkownika';
+  header('Location: index.html?log=5');
+  
+}
 
-    $sql2 = "SELECT Function FROM Users
-	  WHERE Login=? AND Password=?";
-    $stmt2=mysqli_prepare($conn,$sql2);
-    mysqli_stmt_bind_param($stmt2,'ss',$login,$haslo);
+$hash_haslo= ($row["Password"]);
 
-    $sqlu = "SELECT Id_user FROM Users
-    WHERE Login=? AND Password=?";
-     $stmtu=mysqli_prepare($conn,$sqlu);
-     mysqli_stmt_bind_param($stmtu,'ss',$login,$haslo);
+    if (password_verify($haslo, $hash_haslo)) {
+      
 
- try
-        {
-    mysqli_stmt_execute($stmt);
-$res = mysqli_stmt_get_result($stmt);
-
-mysqli_stmt_execute($stmt2);
-$res2 = mysqli_stmt_get_result($stmt2);
-
-mysqli_stmt_execute($stmtu);
-$resu = mysqli_stmt_get_result($stmtu);
-       
-            
-           
-            
-            
-            if(mysqli_num_rows($res)>0)
-            { 
-              echo "Odnaleziono użytkownika<br>";
-
-              $row=mysqli_fetch_array($res2);
-              $row2=mysqli_fetch_array($resu);
+      $sql = "SELECT * FROM Users
+      WHERE Login=?";
+      $stmt=mysqli_prepare($conn,$sql);
+      mysqli_stmt_bind_param($stmt,'s',$login);
+  
+      $sql2 = "SELECT Function FROM Users
+      WHERE Login=? ";
+      $stmt2=mysqli_prepare($conn,$sql2);
+      mysqli_stmt_bind_param($stmt2,'s',$login);
+  
+      $sqlu = "SELECT Id_user FROM Users
+      WHERE Login=?";
+       $stmtu=mysqli_prepare($conn,$sqlu);
+       mysqli_stmt_bind_param($stmtu,'s',$login);
+  
+   try
+          {
+      mysqli_stmt_execute($stmt);
+  $res = mysqli_stmt_get_result($stmt);
+  
+  mysqli_stmt_execute($stmt2);
+  $res2 = mysqli_stmt_get_result($stmt2);
+  
+  mysqli_stmt_execute($stmtu);
+  $resu = mysqli_stmt_get_result($stmtu);
+         
               
+             
               
-              if($row["Function"]=="U"){
-              $id_row2=$row2["Id_user"];
-            //  echo "$id_row2";
-               
-              $_SESSION["userId"]=$id_row2;
-                header('Location: klient.php');//?id='.$id_row2);
-                echo "przekierowanie na strone uzytkownika.<br>";
+              echo(mysqli_num_rows($res));
+              if(mysqli_num_rows($res)>0)
+              { 
+                echo "Odnaleziono użytkownika<br>";
+  
+                $row=mysqli_fetch_array($res2);
+                $row2=mysqli_fetch_array($resu);
+                 $id_row2=$row2["Id_user"];
                 
+                 echo($row["Function"]);
+                if($row["Function"]=="U"){
+               
+              //  echo "$id_row2";
+                 
+                $_SESSION["userId"]=$id_row2;
+                  header('Location: klient.php');//?id='.$id_row2);
+                  echo "przekierowanie na strone uzytkownika.<br>";
+                  
+                
+                }
+                else if($row["Function"]=="P")
+                {
+                  $_SESSION["userId"]=$id_row2;
+                  header('Location: pracownik.php');
+  
+                  echo "przekierowanie na strone pracownika.<br>";
+                }else if($row["Function"]=="A"){
+                  
+                  $_SESSION["userId"]=$id_row2;
+                  header('Location: administrator.php');
+  
+                  echo "przekierowanie na strone administratora.<br>";
+  
+                }else{echo "Przekierowanie nie powiodło się";}
+              }else
+                {
+                echo "Nie odnaleziono użytkownika w bazie <br>";  
+                header('Location: index.html?log=5');
+              }
+              
+             
               
               }
-              else if($row["Function"]=="P")
-              {
-                echo "przekierowanie na strone pracownika.<br>";
-                
-              }else{echo "Przekierowanie nie powiodło się";}
-            }else
-              {
-              echo "Nie odnaleziono użytkownika w bazie <br>";  
-            }
-            
-           
-            
-            }
-        catch(Exception $e){
-          echo "Bład wyszukiwarki lub logowania!<br>";
+          catch(Exception $e){
+            echo "Bład wyszukiwarki lub logowania!<br>";
+            header('Location: index.html?log=5');
+          }
+        }else
+        {
+          header('Location: index.html?log=5');
         }
-       
+         
 
 ?>
--- AGD/nowy_uzytkownik.php
+++ AGD/nowy_uzytkownik.php
@@ -1,35 +1,40 @@
+
 <?php
 include("polaczenie.php");
-
 
 $email = mysqli_real_escape_string($conn,$_POST["email"]);
 $login = mysqli_real_escape_string($conn,$_POST["login"]);
 	$haslo = mysqli_real_escape_string($conn,$_POST["password"]);
+$hash_haslo=password_hash($haslo,PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO Users (Login,Password,Email,Function)
 VALUES (?,?,?,'U')";
 $stmt=mysqli_prepare($conn,$sql);
-mysqli_stmt_bind_param($stmt,'sss',$login,$haslo,$email);
+mysqli_stmt_bind_param($stmt,'sss',$login,$hash_haslo,$email);
 
 
 
 try{
 mysqli_stmt_execute($stmt);
 $res =  mysqli_stmt_get_result($stmt);
+
 }
 catch(Exception $e){
     echo "Bład utworzenia użytkownika<br>";
+    header('Location: index.html?log=5');
   }
 
 
 
 //odpytanie o id dopiero dodanego użytownika i dodanie tego id do drgiej tabeli o nazwie data
 
+
+
 try{
   $sql2 = "SELECT Id_user FROM Users
-  WHERE Login=? AND Password=?";
+  WHERE Login=?";
   $stmt2=mysqli_prepare($conn,$sql2);
-  mysqli_stmt_bind_param($stmt2,'ss',$login,$haslo);
+  mysqli_stmt_bind_param($stmt2,'s',$login);
   mysqli_stmt_execute($stmt2);
 
 
@@ -41,13 +46,19 @@ try{
  $Id=$row["Id_user"];
 
  $sql3 = "INSERT INTO Data(Id_user, Name, Last_name, City, Zip_code, Street, Nr_h, Nr_f, Telephone)
-  VALUES ($Id,'','','','','','','','')";
+  VALUES ($Id,' ',' ',' ',' ',' ',' ',' ',' ')";
    $res = $conn -> query($sql3);
   //$stmt3=mysqli_prepare($conn,$sql3);
  // mysqli_stmt_execute($stmt3);
 
+ header('Location: index.html?log=1');
 
-}catch(Exception $e){echo ("nie mozna dodać id do tabeli data");}
+
+}
+catch(Exception $e){
+  echo ("nie mozna dodać id do tabeli data");
+  header('Location: index.html?log=5');
+}
 echo("uzytkownik zostal dodany");
-
 ?>
+
